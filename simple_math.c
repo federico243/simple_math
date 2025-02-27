9279d8b5d0bcca916292bff0533a2c7843cc5132
federico243 <federico.mbiasioli@gmail.com>
2025-02-27 13:55:32 +0100

    Modifiche

---
+++ simple_math.c
@@ -0,0 +1,10 @@
+/** \file simple_math.h
+ *  \brief a toy example of math library 
+ *  \author Federico Maria Biasioli
+ */
+
+#include "simple_math.h"
+
+float mean(int a, int b){
+    return (a +b)/2;
+}
---
+++ simple_math.h
@@ -0,0 +1,11 @@
+/** \file simple_math.h
+ *  \brief a toy example of math library 
+ *  \author Federico Maria Biasioli
+ */
+
+ #ifndef _SIMPLEMATH_
+ #define _SIMPLEMATH_
+ 
+ float mean (int a, int b);
+ 
+ #endif
